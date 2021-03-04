c93fae6fa06cd0e4e1c29bcd8bd1692dea7e85dc
mhemmer-cern <53471402+mhemmer-cern@users.noreply.github.com>
2021-03-04 18:06:13 +0100

    Delete build/CMakeFiles directory

--- build/CMakeFiles/libB.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblibB.pdb"
-  "liblibB.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libB.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/libC.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblibC.pdb"
-  "liblibC.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
